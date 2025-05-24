e8a84d160c4565cdcc0f9e3858398e53f5e6520e
zhongkunming <73384930+zhongkunming@users.noreply.github.com>
2025-05-25 04:02:16 +0800

    build(supervisor): 更新 Maven构建配置
    - 修改项目版本号为 0.0.1
    - 移除 spring-boot-devtools 和 spring-boot-docker-compose依赖
    - 添加 spring-boot-starter-logging 依赖
    - 更新 .gitignore 文件
    - 重新格式化代码

--- src/test/java/com/unknown/supervisor/SupervisorApplicationTests.java
+++ src/test/java/com/unknown/supervisor/SupervisorApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SupervisorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
