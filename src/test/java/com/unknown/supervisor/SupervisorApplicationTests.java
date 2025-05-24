70851b549cfccddcab3d509322ede238600c482d
zhongkunming <73384930+zhongkunming@users.noreply.github.com>
2025-05-25 03:58:11 +0800

    Initial commit

---
+++ src/test/java/com/unknown/supervisor/SupervisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unknown.supervisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
