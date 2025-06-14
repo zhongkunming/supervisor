4c319fdd4ca1fa651fc806fb805902f2ec268de4
zhongkunming <73384930+zhongkunming@users.noreply.github.com>
2025-06-15 00:45:09 +0800

    feat(supervisor): 添加 MyBatis 分页插件配置
    - 新增 MyBatisConfig 类，配置分页插件
    - 在 application.yaml 中添加 MyBatis相关配置

---
+++ src/main/java/com/unknown/supervisor/config/MyBatisConfig.java
@@ -0,0 +1,23 @@
+package com.unknown.supervisor.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhongkunming
+ */
+@Configuration
+public class MyBatisConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
